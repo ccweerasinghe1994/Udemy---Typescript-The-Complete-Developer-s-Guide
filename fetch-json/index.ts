4a29f5bf6396c48a792ef79ee12e52c0e95b03ef
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-31 18:54:47 +0530

    checking errors with typescript close #4

--- fetch-json/index.ts
+++ fetch-json/index.ts
@@ -2,17 +2,23 @@ import axios from 'axios';
 
 const URL = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
 axios.get(URL).then((response) => {
 	console.log(response.data);
-	const todo = response.data;
+	const todo = response.data as Todo;
 	const id = todo.id;
-	const title = todo.Title;
-	const finished = todo.finished;
+	const title = todo.title;
+	const finished = todo.completed;
 
 	console.log(`
     Todo Item with ID : ${id}
     Has a tile of ${title}
     is it finished ${finished}
-    
+
     `);
 });
