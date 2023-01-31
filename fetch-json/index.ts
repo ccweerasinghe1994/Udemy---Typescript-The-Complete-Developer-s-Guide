1e33a71ed54211f91ba11f9431d6362ee60f0700
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-31 18:47:05 +0530

    close #2

---
+++ fetch-json/index.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const URL = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(URL).then((response) => {
+	console.log(response.data);
+	const todo = response.data;
+	const id = todo.id;
+	const title = todo.Title;
+	const finished = todo.finished;
+
+	console.log(`
+    Todo Item with ID : ${id}
+    Has a tile of ${title}
+    is it finished ${finished}
+    `);
+});
